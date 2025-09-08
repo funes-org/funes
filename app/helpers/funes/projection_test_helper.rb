642b822461953cdeef29b8743e3601866d2a3e07
Vinícius Almeida da Silva <vinialm88@gmail.com>
2025-09-08 20:02:32 -0300

    rename helper function

--- app/helpers/funes/projection_test_helper.rb
+++ app/helpers/funes/projection_test_helper.rb
@@ -1,6 +1,6 @@
 module Funes
   module ProjectionTestHelper
-    def project_event_based_on(projection_class, event_instance, previous_state)
+    def interpret_event_based_on(projection_class, event_instance, previous_state)
       projection_class.interpretations[event_instance.class]
                       .call(previous_state, event_instance)
     end
