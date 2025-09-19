4e4664a17c89af93c2a05a32a98ff0ca64bd8ccf
Vinícius Almeida da Silva <vinialm88@gmail.com>
2025-09-18 22:15:32 -0300

    Add TODO

--- app/helpers/funes/projection_test_helper.rb
+++ app/helpers/funes/projection_test_helper.rb
@@ -1,5 +1,6 @@
 module Funes
   module ProjectionTestHelper
+    # @TODO add test coverage
     def interpret_event_based_on(projection_class, event_instance, previous_state)
       projection_class.interpretations[event_instance.class]
                       .call(previous_state, event_instance)
